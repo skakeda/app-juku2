257f41af18de4b1633a8ea0cf1aa82f6ac514434
skakeda <108124983+skakeda@users.noreply.github.com>
2022-10-17 09:57:04 +0900

    skakeda_changes
    listen and serveのポートを変更
    func skakedaを追加

--- main.go
+++ main.go
@@ -39,4 +39,62 @@ func myamagata(w http.ResponseWriter, r *http.Request) {
     t.Execute(w, body)
 }
 
+func main() {
+    http.HandleFunc("/myamagata", myamagata)
+    http.ListenAndServe(":8003", nil)
+}
 
+func myamagata(w http.ResponseWriter, r *http.Request) {
+    db, err := sql.Open("mysql", "myamagata:myamagata_App_2022@tcp(10.35.0.59:3306)/seweb")
+                if err != nil {
+                        panic(err.Error())
+                }
+    defer db.Close()
+
+     rows, err := db.Query("SELECT body FROM myamagata_web WHERE id=1")
+                if err != nil {
+                        panic(err.Error())
+                }
+
+    defer rows.Close()
+
+    var body string
+    rows.Next()
+    rows.Scan(&body)
+// log.Print(body)
+    t , err := template.ParseFiles("myamagata.tpl")
+                if err != nil {
+                        panic(err.Error())
+                }
+    t.Execute(w, body)
+}
+
+func main() {
+    http.HandleFunc("/skakeda", skakeda)
+    http.ListenAndServe(":8008", nil)
+}
+
+func skakeda(w http.ResponseWriter, r *http.Request) {
+    db, err := sql.Open("mysql", "skakeda:skakeda_App_2022@tcp(10.35.0.59:3306)/seweb")
+                if err != nil {
+                        panic(err.Error())
+                }
+    defer db.Close()
+
+     rows, err := db.Query("SELECT body FROM skakeda_web WHERE id=1")
+                if err != nil {
+                        panic(err.Error())
+                }
+
+    defer rows.Close()
+
+    var body string
+    rows.Next()
+    rows.Scan(&body)
+// log.Print(body)
+    t , err := template.ParseFiles("skakeda.tpl")
+                if err != nil {
+                        panic(err.Error())
+                }
+    t.Execute(w, body)
+}
