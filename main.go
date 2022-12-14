5add037dc53b4224595a4da14b9e64a7477b0e0c
skakeda <108124983+skakeda@users.noreply.github.com>
2022-12-15 08:51:36 +0900

    Update main.go
    add skakeda func

--- main.go
+++ main.go
@@ -10,12 +10,39 @@ import(
 )
 
 func main() {
+    http.HandleFunc("/skakeda", skakeda)
     http.HandleFunc("/tozono", tozono)
     http.HandleFunc("/myamagata", myamagata)
     http.HandleFunc("/yichikawa", yichikawa)
     http.HandleFunc("/syatani", syatani)
     http.ListenAndServe(":8003", nil)
 
+}
+
+
+func skakeda(w http.ResponseWriter, r *http.Request) {
+    db, err := sql.Open("mysql", "skakeda:skakeda_App_2022@tcp(10.35.0.59:3306)/seweb")
+                if err != nil {
+                        panic(err.Error())
+                }
+    defer db.Close()
+
+     rows, err := db.Query("SELECT body FROM skakeda_web WHERE id=1")
+                if err != nil {
+                        panic(err.Error())
+                }
+
+    defer rows.Close()
+
+    var body string
+    rows.Next()
+    rows.Scan(&body)
+// log.Print(body)
+    t , err := template.ParseFiles("main.tpl")
+                if err != nil {
+                        panic(err.Error())
+                }
+    t.Execute(w, body)
 }
 
 func tozono(w http.ResponseWriter, r *http.Request) {
